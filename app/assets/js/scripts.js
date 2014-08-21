29b0ccfba21d6cfe7b9687671816ae47cd8c65d2
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2014-08-21 15:24:14 +0200

    if is android 4.1.2

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -15,8 +15,6 @@
 	function deviceOS() {
 		if(useragent.match(/Android/i)) {
 			return 'android';
-		} else if(useragent.match(/Android 4.4.4/i)) {
-			return 'android 4.4.4';
 		} else if(useragent.match(/iPhone/i)) {
 			return 'iphone';
 		} else if(useragent.match(/iPod/i)) {
@@ -32,10 +30,10 @@
 
 	var androidTypes = {
 		number : [
-			"4.1.2", "4.2.2", "4.4.4", "2.3.3"
+			"4.2.2", "4.4.4", "4.4.2", "2.3.3"
 		],
 		tel : [
-			"2.3", "2.3", "2.3"
+			"4.1.2"
 		]
 	}
 
@@ -55,6 +53,15 @@
 		        	input[i].type = 'tel';
 		        	console.log(input[i], 'Changed to tel');
 			    }
+			}
+		} else if(deviceOS() === 'android') {
+			if(useragent.match(/Android 4.1.2/i)) {
+				for(var i in input){
+				    if(input[i].type === 'number'){
+			        	input[i].type = 'tel';
+			        	console.log(input[i], 'Changed to number');
+				    }
+				}
 			}
 		}
 	}
