f9e42143877802187ba7696ea28cd773d9a885bd
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2014-08-21 12:30:21 +0200

    Created the test

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,46 @@
 (function () {
 
-  'use strict';
+	'use strict';
 
-  
+	function deviceOS() {
+		var useragent = navigator.userAgent;
+		if(useragent.match(/Android/i)) {
+			return 'android';
+		} else if(useragent.match(/webOS/i)) {
+			return 'webos';
+		} else if(useragent.match(/iPhone/i)) {
+			return 'iphone';
+		} else if(useragent.match(/iPod/i)) {
+			return 'ipod';
+		} else if(useragent.match(/iPad/i)) {
+			return 'ipad';
+		} else if(useragent.match(/Windows Phone/i)) {
+			return 'windows phone';
+		} else if(useragent.match(/SymbianOS/i)) {
+			return 'symbian';
+		} else if(useragent.match(/RIM/i) || useragent.match(/BB/i)) {
+			return 'blackberry';
+		} else {
+			return 'no-device';
+		}
+	}
+
+	function setRightInputKeyboard() {
+		var input = document.getElementsByTagName('input');
+		if(deviceOS() === 'iphone' || deviceOS() === 'ipod' || deviceOS() === 'ipad') {
+			console.log('deviceOS: ', deviceOS());
+			for(var i in input){
+			    if(input[i].type == "number"){
+			        if() {
+			        	input[i].type = 'tel';
+			        	console.log(input[i], 'Changed to tel');
+			        }
+			    }
+			}
+		}
+	}
+
+	setRightInputKeyboard();
+
 
 })();
